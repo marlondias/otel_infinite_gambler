26439f2362d79e625f44da27cd758bd7e0779bf6
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-06-29 18:52:54 -0300

    feat: Added basic setup for OTel logs and metrics

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+var oTelCollectorEndpoint = new Uri("http://localhost:4318");
+var oTelCollectorProtocol = OtlpExportProtocol.HttpProtobuf;
+
+var meterProvider = Sdk.CreateMeterProviderBuilder()
+    // Other setup code, like setting a resource goes here too
+    .AddOtlpExporter(options =>
+    {
+        options.Endpoint = oTelCollectorEndpoint;
+        options.Protocol = oTelCollectorProtocol;
+    })
+    .Build();
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddOpenTelemetry(logging =>
+    {
+        logging.AddOtlpExporter(options =>
+        {
+            options.Endpoint = oTelCollectorEndpoint;
+            options.Protocol = oTelCollectorProtocol;
+        });
+    });
+});
+
+Console.WriteLine("Hello, World!");
