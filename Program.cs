533366e4ed5cf311125ee27c235284acc16cfe00
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-06-30 01:07:49 -0300

    feat: Working demo for logs and metrics propagation.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics.Metrics;
+using Microsoft.Extensions.Logging;
 using OpenTelemetry;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
-using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
 
-var oTelCollectorEndpoint = new Uri("http://localhost:4318");
-var oTelCollectorProtocol = OtlpExportProtocol.HttpProtobuf;
+var oTelCollectorEndpoint = new Uri("http://localhost:4317");
+var oTelCollectorProtocol = OtlpExportProtocol.Grpc;
+var serviceName = "Infinite_Gambler_OTel";
+var resourceBuilder = ResourceBuilder.CreateDefault().AddService(serviceName);
 
 var meterProvider = Sdk.CreateMeterProviderBuilder()
-    // Other setup code, like setting a resource goes here too
-    .AddOtlpExporter(options =>
+    .SetResourceBuilder(resourceBuilder)
+    .AddMeter(serviceName)
+    .AddOtlpExporter((exporterOptions, readerOptions) =>
     {
-        options.Endpoint = oTelCollectorEndpoint;
-        options.Protocol = oTelCollectorProtocol;
+        exporterOptions.Endpoint = oTelCollectorEndpoint;
+        exporterOptions.Protocol = oTelCollectorProtocol;
+        readerOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 5000;
     })
     .Build();
 
@@ -21,6 +26,9 @@ var loggerFactory = LoggerFactory.Create(builder =>
 {
     builder.AddOpenTelemetry(logging =>
     {
+        logging.SetResourceBuilder(resourceBuilder);
+        logging.IncludeFormattedMessage = true;
+        logging.IncludeScopes = true;
         logging.AddOtlpExporter(options =>
         {
             options.Endpoint = oTelCollectorEndpoint;
@@ -29,4 +37,19 @@ var loggerFactory = LoggerFactory.Create(builder =>
     });
 });
 
-Console.WriteLine("Hello, World!");
+// =====
+
+var logger = loggerFactory.CreateLogger(serviceName);
+var meter = new Meter(serviceName);
+var loopCounter = meter.CreateCounter<long>("example.loop.iterations", description: "Total iteration in main loop.");
+
+long iteration = 0;
+while (iteration++ < long.MaxValue)
+{
+    logger.LogInformation($"Loop iteration {iteration} started.");
+    loopCounter.Add(1);
+    await Task.Delay(100);
+}
+
+meterProvider.Dispose();
+loggerFactory.Dispose();
