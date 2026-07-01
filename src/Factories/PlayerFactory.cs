d8cb900fdff2adad4273f00bc9aa9dd44b42e6bb
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-07-01 11:37:45 -0300

    feat: Added logger to Player factory

--- src/Factories/PlayerFactory.cs
+++ src/Factories/PlayerFactory.cs
@@ -1,14 +1,25 @@
 namespace InfiniteGambler.Factories;
 
 using Bogus;
+using InfiniteGambler.Constants;
 using InfiniteGambler.Entities;
+using Microsoft.Extensions.Logging;
 
-public class PlayerFactory(Faker faker)
+public class PlayerFactory(ILogger logger, Faker faker)
 {
+    private readonly ILogger _logger = logger;
     private readonly Faker _faker = faker;
 
     public Player Create()
     {
-        return new Player { Name = _faker.Name.FullName() };
+        var player = new Player { Name = _faker.Name.FullName() };
+        var minInitialCash = GameFactoryConstants.MaxBetCost * 1000;
+        player.AddCashBalance(_faker.Random.Decimal(minInitialCash, minInitialCash * 1000));
+
+        _logger.LogInformation(
+            $"A player was created. Name={player.Name} CashBalance={player.CashBalance}."
+        );
+
+        return player;
     }
 }
