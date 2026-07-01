407ba588440f2d2baf88bc65021da49ea5398419
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-07-01 01:46:35 -0300

    fix: Changed Games to an immutable type

--- src/Entities/Casino.cs
+++ src/Entities/Casino.cs
@@ -1,9 +1,11 @@
+using System.Collections.Immutable;
+
 namespace InfiniteGambler.Entities;
 
 public class Casino
 {
     public required string Name { get; init; }
-    public required Game[] Games { get; init; }
+    public required ImmutableArray<Game> Games { get; init; }
     public decimal PurchasePrice
     {
         get => Games.Max(g => g.Payout) * 1.5m;
