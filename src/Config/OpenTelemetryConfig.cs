63cece91fb5a078e41d56b3cafeed41daec0b7d1
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-07-01 18:24:53 -0300

    fix: Added missing key for tuning the export interval

--- src/Config/OpenTelemetryConfig.cs
+++ src/Config/OpenTelemetryConfig.cs
@@ -2,4 +2,8 @@ namespace InfiniteGambler.Config;
 
 public record OpenTelemetryCollectorConfig(string Endpoint, bool UseGrpc);
 
-public record OpenTelemetryConfig(string ServiceName, OpenTelemetryCollectorConfig Collector);
+public record OpenTelemetryConfig(
+    string ServiceName,
+    int MetricsExportIntervalInMS,
+    OpenTelemetryCollectorConfig Collector
+);
