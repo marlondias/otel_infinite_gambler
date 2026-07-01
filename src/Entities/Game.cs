f95ade3ab1d66f862b2ad99f9646e9861e82ef1e
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-06-30 22:09:37 -0300

    feat: Added VSCode config for step debugging

---
+++ src/Entities/Game.cs
@@ -0,0 +1,14 @@
+namespace InfiniteGambler.Entities;
+
+public class Game
+{
+    // This class must have public properties for:
+    // > Game's name
+    // > The odds of winning the game. A floating number between 0 and 1, but never 0.
+    // > The cost for betting on this game (participation fee, ticket price, etc).
+    // > The total value of the prize.
+
+    // Choose the property names following best practices and considering the context of Casino, gambling, games of chance, bets.
+    // These properties will probably never be changed after instantiation, consider that as well.
+    // No constructor is necessary.
+}
