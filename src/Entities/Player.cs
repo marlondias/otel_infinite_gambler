3a550005e37e10a9cdeadec8183b9c63d581fe3b
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-07-01 12:09:34 -0300

    feat: New methods for updating cash balance

--- src/Entities/Player.cs
+++ src/Entities/Player.cs
@@ -4,4 +4,20 @@ public class Player
 {
     public required string Name { get; init; }
     public decimal CashBalance { get; private set; }
+
+    public void AddCashBalance(decimal amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Amount must be positive.");
+
+        CashBalance += amount;
+    }
+
+    public void SubtractFromCashBalance(decimal amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Amount must be positive.");
+
+        CashBalance -= amount;
+    }
 }
