b1c1bca6b850dac56859e47854e8fbfd2bcaf20b
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-07-02 14:22:54 -0300

    feat: New method for playing a single game

--- src/Entities/Casino.cs
+++ src/Entities/Casino.cs
@@ -10,9 +10,36 @@ public class Casino
     {
         get => Games.Max(g => g.Payout) * 1.5m;
     }
+    public decimal MoneyReceivedInBets { get; private set; }
+    public decimal MoneyGivenInPrizes { get; private set; }
 
     public int[] GetIndexesOfGamesWithinBudget(decimal budget)
     {
         return Games.Index().Where(x => x.Item.BetCost <= budget).Select(x => x.Index).ToArray();
+    }
+
+    public void PlayGame(int gameIndex, Player player)
+    {
+        if (gameIndex < 0 || gameIndex > Games.Length)
+            throw new ArgumentOutOfRangeException(
+                $"Index of game is invalid in this casino. GameIndex={gameIndex}"
+            );
+
+        if (player.CashBalance < Games[gameIndex].BetCost)
+            throw new InvalidOperationException(
+                "Player does not have enough money to play this game."
+            );
+
+        Games[gameIndex].IncrementBetsCount();
+        player.SubtractFromCashBalance(Games[gameIndex].BetCost);
+        MoneyReceivedInBets += Games[gameIndex].BetCost;
+
+        bool isWinner = Random.Shared.NextDouble() < Games[gameIndex].Odds;
+        if (isWinner)
+        {
+            Games[gameIndex].IncrementWinnersCount();
+            MoneyGivenInPrizes += Games[gameIndex].Payout;
+            player.AddCashBalance(Games[gameIndex].Payout);
+        }
     }
 }
