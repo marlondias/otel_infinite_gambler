d3c9098fa7c79eda933b3448040e6fcf1297312c
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-07-02 21:47:06 -0300

    fix: Reduced range for initial cash of players

--- src/Constants/PlayerFactoryConstants.cs
+++ src/Constants/PlayerFactoryConstants.cs
@@ -5,5 +5,5 @@ public static class PlayerFactoryConstants
     private const decimal AverageBetCost =
         (GameFactoryConstants.MaxBetCost + GameFactoryConstants.MinBetCost) / 2;
     public const decimal MinInitialCash = AverageBetCost * 1000;
-    public const decimal MaxInitialCash = MinInitialCash * 100;
+    public const decimal MaxInitialCash = MinInitialCash * 10;
 }
